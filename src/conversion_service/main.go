dad616fd816b37a1738d69544d88cce05fa5a34d
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-03-08 13:18:00 +0530

    Refactor the media conversion code

--- src/conversion_service/main.go
+++ src/conversion_service/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	log "github.com/sirupsen/logrus"
 	"github.com/thejasmeetsingh/spotify-clone/src/conversion_service/pb"
 	"google.golang.org/grpc"
@@ -58,11 +63,108 @@ func main() {
 	}
 }
 
+// gRPC request handler
 func (s *server) Conversion(ctx context.Context, in *pb.ConversionRequest) (*pb.ConversionResponse, error) {
 	// Convert the media file in background
-	go ConvertMediaFile(in.GetKey(), in.GetIsAudioFile())
+	go convertMediaFile(in.GetKey(), in.GetIsAudioFile())
 
 	return &pb.ConversionResponse{
 		Message: "Request received successfully, Processing media file",
 	}, nil
+}
+
+func getS3Client() (*s3.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(os.Getenv("AWS_REGION")))
+	if err != nil {
+		return nil, err
+	}
+	// Create an Amazon S3 service client
+	return s3.NewFromConfig(cfg), nil
+}
+
+func downloadFile(client *s3.Client, bucket, key, downloadPath string) error {
+	// Fetch the file from s3
+	result, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return err
+	}
+	defer result.Body.Close()
+
+	// Create a file to write the download to
+	file, err := os.Create(downloadPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write the contents of S3 Object to the file
+	if _, err = file.ReadFrom(result.Body); err != nil {
+		return err
+	}
+
+	log.Infof("%s object downloaded successfully", key)
+	return nil
+}
+
+func uploadFile(client *s3.Client, bucket, key, filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln("error while opening the given file: ", err)
+		return
+	}
+
+	// Upload the given file to s3
+	if _, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	}); err != nil {
+		log.Fatalln("error caught while uploading file to s3: ", err)
+		return
+	}
+
+	log.Infof("%s object uploaded successfully", key)
+	file.Close()
+
+	if err = os.Remove(filePath); err != nil {
+		log.Errorln("error caught while deleting the file", err)
+	}
+}
+
+func convertMediaFile(key string, isAudioFile bool) {
+	client, err := getS3Client()
+	if err != nil {
+		log.Fatalln("error caught while creating s3 client: ", err)
+		return
+	}
+
+	bucket := os.Getenv("AWS_BUCKET")
+	downloadPath := fmt.Sprintf("/media/%s", key)
+	outputPath := fmt.Sprintf("/output/%s", strings.Split(key, ".")[0]+".m3u8")
+
+	// Download the file from s3
+	downloadFile(client, bucket, key, downloadPath)
+
+	var convertCmd *exec.Cmd
+
+	// Determine conversion command based on file type
+	if isAudioFile {
+		// FFmpeg command for converting audio to AAC and then to HLS
+		convertCmd = exec.Command("ffmpeg", "-i", downloadPath, "-c:a", "aac", "-b:a", "320k", "-vn", "-hls_time", "10", "-hls_playlist_type", "vod", outputPath)
+	} else {
+		// FFmpeg command for converting video to H.265/HEVC and then to HLS
+		convertCmd = exec.Command("ffmpeg", "-i", downloadPath, "-c:v", "libx265", "-crf", "28", "-c:a", "aac", "-b:a", "320k", "-hls_time", "10", "-hls_playlist_type", "vod", outputPath)
+	}
+
+	// Execute conversion
+	if err := convertCmd.Run(); err != nil {
+		log.Fatalln("error caught while converting the media file: ", err)
+		return
+	}
+
+	// Upload file back to s3 in background
+	go uploadFile(client, bucket, key, outputPath)
 }
